3326808a5d83c4b20051c453c5ea29ea8506a207
luisejrobles <luisejimenezrobles@gmail.com>
2017-03-30 21:51:41 -0700

    Cambios raros en archivos generados por AVR Studio

--- AvrBuild.bat
+++ AvrBuild.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"D:\Program Files (x86)\AVRStudio4\AvrAssembler2\avrasm2.exe" -S "C:\Micros\P6\labels.tmp" -fI -W+ie -o "C:\Micros\P6\P6.hex" -d "C:\Micros\P6\P6.obj" -e "C:\Micros\P6\P6.eep" -m "C:\Micros\P6\P6.map" "C:\Micros\P6\p6.asm"
+"D:\Program Files (x86)\AVRStudio4\AvrAssembler2\avrasm2.exe" -S "C:\Micros\P6\labels.tmp" -fI -W+ie -o "C:\Micros\P6\P6.hex" -d "C:\Micros\P6\P6.obj" -e "C:\Micros\P6\P6.eep" -m "C:\Micros\P6\P6.map" "C:\Micros\P6\test.asm"
